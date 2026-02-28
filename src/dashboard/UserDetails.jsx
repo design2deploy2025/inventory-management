427aadce8847a66d046133efbcc74fbc063ed7d8
prathamm2506 <fs20if005@gmail.com>
2026-02-28 09:40:19 +0530

    pre version 1

--- src/dashboard/UserDetails.jsx
+++ src/dashboard/UserDetails.jsx
@@ -23,7 +23,7 @@ export default function UserDetails() {
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm/6 font-medium text-gray-100">Salary expectation</dt>
-            <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">$120,000</dd>
+<dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">₹120,000</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm/6 font-medium text-gray-100">About</dt>
